7ef98cd19cb232af3c827ada0371ece01b630b4e
Alyssa Harris <ah00214@my.westga.edu>
2021-06-22 16:59:18 -0400

    Working in getting components to route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HelloComponent } from './app/tournament/hello/hello.component';
-import { BracketsComponent } from './app/tournament/brackets/brackets.component';
-import { RegistrationComponent } from './app/tournament/registration/registration.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HelloComponent,
-    BracketsComponent,
-    RegistrationComponent
+    routingComponents
   ],
   imports: [
     BrowserModule,
