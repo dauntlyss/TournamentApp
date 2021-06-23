9fb5158687cd191c24491386105604dd29f5a8d6
Alyssa Harris <ah00214@my.westga.edu>
2021-06-23 16:48:14 -0400

    Added functionality to brackets page

--- src/app/tournament/brackets/brackets.component.ts
+++ src/app/tournament/brackets/brackets.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RosterService } from '../../services/roster.service';
 
 @Component({
   selector: 'app-brackets',
@@ -6,10 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./brackets.component.css']
 })
 export class BracketsComponent implements OnInit {
+  public players: String[];
+  public message;
+  public winners: String[];
+  public matchOne: String[];
 
-  constructor() { }
+  constructor(private rosterService: RosterService) { }
 
   ngOnInit(): void {
+    this.players = this.rosterService.getContestants();
+	  this.message = null;
+    this.winners = ['','','',''];
+    this.matchOne = [this.players[0], this.players[1]];
+  }
+
+  setMatchOneWinner (winner: String) {
+  	this.winners[0] = winner;
+  }
+
+  completeRound() {
+    this.message = this.matchOne;
+    for (let winner in this.winners) {
+      if (this.winners[winner].trim() == '') {
+        continue;
+      }
+    }
+    this.message = this.winners.toString();
   }
 
 }
